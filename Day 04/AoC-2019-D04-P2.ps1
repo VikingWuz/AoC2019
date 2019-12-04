780ed765f8a491ac3b3f52b5d953c0a24eb49557
VikingWuz <45904496+VikingWuz@users.noreply.github.com>
2019-12-04 22:15:24 +0100

    Add files via upload

---
+++ Day 04/AoC-2019-D04-P2.ps1
@@ -0,0 +1,90 @@
+$Range = Get-Content 'AoC-2019-D04-Input.txt'
+
+[int]$RangeLow = ($Range.split("-"))[0]
+[int]$RangeHigh = ($Range.split("-"))[1]
+$PossiblePasswords = 0
+
+function NotPartOfLargerGroup($Number) {
+    [String]$Number = $Number
+    $HashTable = @{}
+    1..9 | ForEach-Object {
+        $HashTable[$_] = 0
+    }
+    for($i = 0; $i -lt $Number.Length; $i++) {
+        if($HashTable.ContainsKey([int][string]$Number[$i])) {
+            $HashTable[[int][string]$Number[$i]]++
+        }       
+    }
+    if($HashTable.ContainsValue(2)) {
+        return $true
+    } else {
+        return $false
+    }
+}
+
+function IsAdjacent($Number) {
+    $HasAdjacentNumber = $false
+    [string]$Number = $Number
+    for ($i = 1; $i -lt $number.Length; $i++) {
+        if (($Number[$i] - $Number[$i - 1]) -eq 0) {
+            if (NotPartOfLargerGroup -Number $Number) {
+                $HasAdjacentNumber = $true
+                break
+            }            
+        }
+    }
+    if ($HasAdjacentNumber) {
+        #Write-Host "Sequence has adjacent numbers"
+        return $true
+    } else {
+        #Write-Host "Sequence DOESNT has adjacent numbers"
+        return $false
+    }
+}
+
+function Decreases($Number) {
+    $NeverDecreases = $true
+    [string]$Number = $Number
+    for ($i = 1; $i -lt $number.Length; $i++) {
+        if (($Number[$i] - $Number[$i - 1]) -lt 0) {
+            $NeverDecreases = $false
+            break
+        }
+    }
+    if ($NeverDecreases -eq $true) {
+        #Write-Host "Sequence never decreases"
+        return $true
+    } else {
+        #Write-Host "Sequence decreases"
+        return $false
+    }
+}
+
+function CheckNumber($Number) {
+    if (IsAdjacent -Number $Number) {
+        if(Decreases -Number $Number) {
+            return $true
+        } else {
+            return $false
+        }
+    } else {
+        return $false
+    }
+}
+
+$TotalPossibilietiesToCheck = $RangeHigh - $RangeLow + 1
+Write-Host "Checking $TotalPossibilietiesToCheck possibilities"
+$Counter = 0
+
+for($i = $RangeLow; $i -le $RangeHigh ; $i++){
+    $Counter++
+    if (($Counter % 50000) -eq 0) {
+        Write-Host "Checked $Counter possibilities"
+    }
+    if (CheckNumber -Number $i) {
+        $PossiblePasswords++
+    }
+}
+
+Write-Host "$PossiblePasswords possible passwords found"
+
